de094899332b0a458026539907798eaa903d331c
dannyl <danilovlaurent@pursuit.org>
2021-02-14 22:01:20 -0500

    refactor auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -4,23 +4,37 @@ const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const usersStore = require("../store/users");
 const validateWith = require("../middleware/validation");
+const firebase = require("../firebase");
 
-const schema = {
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(5),
+// const schema = {
+//   email: Joi.string().email().required(),
+//   password: Joi.string().required().min(5),
+// };
+
+// router.post("/", validateWith(schema), (req, res) => {
+//   const { email, password } = req.body;
+//   const user = usersStore.getUserByEmail(email);
+//   if (!user || user.password !== password)
+//     return res.status(400).send({ error: "Invalid email or password." });
+
+//   const token = jwt.sign(
+//     { userId: user.id, name: user.name, email },
+//     "jwtPrivateKey"
+//   );
+//   res.send(token);
+// });
+
+const checkFireBaseToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authtoken;
+    const decodedToken = await firebase.auth().verifyIdToken(token);
+    const uid = decodedToken.uid;
+    req.uid = uid;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "No authenticated user" });
+  }
 };
 
-router.post("/", validateWith(schema), (req, res) => {
-  const { email, password } = req.body;
-  const user = usersStore.getUserByEmail(email);
-  if (!user || user.password !== password)
-    return res.status(400).send({ error: "Invalid email or password." });
-
-  const token = jwt.sign(
-    { userId: user.id, name: user.name, email },
-    "jwtPrivateKey"
-  );
-  res.send(token);
-});
-
-module.exports = router;
+module.exports = checkFireBaseToken;
